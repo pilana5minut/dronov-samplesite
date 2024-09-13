e802051b86366db0fc92c8003239425773e642e5
Maxim <mogilevskiymv@gmail.com>
2024-09-13 16:52:06 +0300

    change settings for database

--- samplesite/samplesite/settings.py
+++ samplesite/samplesite/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'samplesite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': '/mnt/d/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': '/mnt/d/db.sqlite3',
     }
 }
 
